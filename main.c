0d6d0354a42beb50207717794fcacb4cd59dada8
Trung Ngo <ndtrung4419@gmail.com>
2013-11-15 00:27:52 +0700

    Test the new Mark functions

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <wchar.h>
 
 #include "tone.h"
+#include "mark.h"
 
 int main(int argc, char const *argv[])
 {
@@ -22,6 +23,12 @@ int main(int argc, char const *argv[])
     printf("%ls\n", add_tone_to_string(L"khuyu", HOOK));
     printf("%ls\n", add_tone_to_string(L"huyên", HOOK));
     printf("%ls\n", add_tone_to_string(L"hương", HOOK));
+
+    printf("%lc\n", strip_mark_from_char(L'ư'));
+    printf("%lc\n", add_mark_to_char(L'u', MARK_HORN));
+
+    printf("%ls\n", add_mark_to_string(L"tuong", MARK_HORN));
+    printf("%ls\n", add_mark_to_string(L"tuơng", MARK_HORN));
 
     return 0;
 }
