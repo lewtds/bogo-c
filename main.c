cc95f2b75e1736a2447238405b3c8f76f73fac18
Trung Ngo <ndtrung4419@gmail.com>
2013-11-14 19:56:35 +0700

    Testing with main()

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <locale.h>
 #include <stdlib.h>
+#include <wchar.h>
+
 #include "tone.h"
 
 int main(int argc, char const *argv[])
 {
-    char buff[32];
-    wctomb(buff, add_tone_to_char(L'a', ACUTE));
+    if (!setlocale(LC_CTYPE, "")) {
+      fprintf(stderr, "Can't set the specified locale! "
+              "Check LANG, LC_CTYPE, LC_ALL.\n");
+      return 1;
+    }
 
-    printf("%s\n", buff);
+    wchar_t *input = L"con";
+    wchar_t *output = add_tone_to_string(input, GRAVE);
+
+
+    printf("%ls\n", output);
     return 0;
 }
