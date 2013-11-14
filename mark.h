8a5f2ea381f175b4c34bb1e3fdf5b49fce3783e9
Trung Ngo <ndtrung4419@gmail.com>
2013-11-15 00:27:27 +0700

    Change the order of Mark enum

--- mark.h
+++ mark.h
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 
 typedef enum {
+    MARK_NONE,
     MARK_HAT,
     MARK_BREVE,
     MARK_HORN,
-    MARK_DASH,
-    MARK_NONE
+    MARK_DASH
 } Mark;
 
 wchar_t strip_mark_from_char(wchar_t chr);
